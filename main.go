bdef1a0b9711770f8b660dc019413e8d87a8b9c1
kartmatias <kartmatias@gmail.com>
2019-12-04 22:46:32 -0300

    Inicio API bravo

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/middleware/logger"
+	"github.com/kataras/iris/middleware/recover"
+)
+
+func main() {
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+	app.Use(recover.New())
+	app.Use(logger.New())
+
+	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
+}
